5a9744e235c40d802a7d5ede5c18c563a1a99c03
Alexander Kochetov <creocoder@gmail.com>
2015-03-20 23:29:44 +0300

    Replaced sources file header

--- src/TypeAheadAsset.php
+++ src/TypeAheadAsset.php
@@ -1,13 +1,19 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-type-ahead-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\typeahead;
 
 use yii\web\AssetBundle;
 
+/**
+ * TypeAheadAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
 class TypeAheadAsset extends AssetBundle
 {
 	public $sourcePath = '@vendor/2amigos/yii2-type-ahead-widget/assets';
--- src/TypeAheadPluginAsset.php
+++ src/TypeAheadPluginAsset.php
@@ -1,13 +1,19 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
- * @link http://2amigos.us
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link https://github.com/2amigos/yii2-type-ahead-widget
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
  */
+
 namespace dosamigos\typeahead;
 
 use yii\web\AssetBundle;
 
+/**
+ * TypeAheadPluginAsset
+ *
+ * @author Alexander Kochetov <creocoder@gmail.com>
+ */
 class TypeAheadPluginAsset extends AssetBundle
 {
 	public $sourcePath = '@vendor/twitter/typeahead.js/dist';
